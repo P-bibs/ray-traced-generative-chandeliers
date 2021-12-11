19719a59f04665ea0f3edb4b14f33fa6e46a13f6
Paul Biberstein <biberstein.paul@gmail.com>
2021-12-11 01:45:28 -0500

    Improve chain modularity

--- generation/components/Chain.py
+++ generation/components/Chain.py
@@ -1,28 +1,49 @@
-from shapes.primitives import Sphere
+from shapes.primitives import Cube, Sphere
 from common import SceneComponent
 from settings import settings
 from shapes.transblock import TransBlock
 from shapes.tree import Tree
 
 
-def SimpleChain(top, count, radius):
-    return TaperedChain(top, count, radius, radius)
-
-class TaperedChain(SceneComponent):
-    def __init__(self, top, count, start_radius, end_radius):
-        self.top = top
-        self.count = count
-        self.start_radius = start_radius
-        self.end_radius = end_radius
-
-    def scene_rep(self):
-        sphere = Sphere(
+def SphereChain(top, count, radius):
+    sphere = Sphere(
             diffuse=settings.default_diffuse,
             ambient=settings.default_ambient,
             specular=settings.default_specular,
             shininess=settings.default_shininess,
             refraction=settings.default_refraction,
         )
+    return TaperedChain(sphere, top, count, radius, radius)
+
+def TaperedSphereChain(top, count, start_radius, end_radius):
+    sphere = Sphere(
+        diffuse=settings.default_diffuse,
+        ambient=settings.default_ambient,
+        specular=settings.default_specular,
+        shininess=settings.default_shininess,
+        refraction=settings.default_refraction,
+    )
+    return TaperedChain(sphere, top, count, start_radius, end_radius)
+
+def DiamondChain(top, count, radius):
+    diamond = Tree([TransBlock(Cube(
+        diffuse=settings.default_diffuse,
+        ambient=settings.default_ambient,
+        specular=settings.default_specular,
+        shininess=settings.default_shininess,
+        refraction=settings.default_refraction,
+    ), rotate=(-1,-1,0,90))])
+    return TaperedChain(diamond, top, count, radius / 2, radius / 2)
+
+class TaperedChain(SceneComponent):
+    def __init__(self, shape, top, count, start_radius, end_radius):
+        self.shape = shape
+        self.top = top
+        self.count = count
+        self.start_radius = start_radius
+        self.end_radius = end_radius
+
+    def scene_rep(self):
 
         links = []
         current_y_location = 0
@@ -33,7 +54,7 @@ class TaperedChain(SceneComponent):
             current_y_location -= interped_radius
             links.append(
                 TransBlock(
-                    sphere,
+                    self.shape,
                     translate=(0, current_y_location, 0),
                     scale=(interped_radius, interped_radius, interped_radius),
                 )
