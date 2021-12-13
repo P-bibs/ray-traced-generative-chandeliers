7018b709a23cb4fb09af909580aed54ae00c9205
Paul Biberstein <biberstein.paul@gmail.com>
2021-12-13 11:50:42 -0500

    Add tilted chain

--- generation/components/Chain.py
+++ generation/components/Chain.py
@@ -5,6 +5,42 @@ from shapes.transblock import TransBlock
 from shapes.tree import Tree
 
 
+class TiltedSphereChain(SceneComponent):
+    def __init__(self, start, end, count):
+        self.start = start
+        self.end = end
+        self.count = count
+
+    def scene_rep(self):
+        sphere = Sphere(
+        diffuse=settings.default_diffuse,
+        ambient=settings.default_ambient,
+        specular=settings.default_specular,
+        shininess=settings.default_shininess,
+        refraction=settings.default_refraction,
+    )
+        start_to_end = (self.end[0] - self.start[0], self.end[1] - self.start[1], self.end[2] - self.start[2])
+
+        distance = (start_to_end[0] ** 2 + start_to_end[1] ** 2 + start_to_end[2] ** 2) ** 0.5
+        radius = distance / (2 * self.count)
+
+        spheres = []
+        for i in range(self.count):
+            progress = (i + 0.5) / self.count
+            interped = (
+                self.start[0] + start_to_end[0] * progress,
+                self.start[1] + start_to_end[1] * progress,
+                self.start[2] + start_to_end[2] * progress
+            )
+            spheres.append(TransBlock(
+                sphere, translate=(interped), scale=(radius, radius, radius)
+            ))
+
+        return TransBlock(Tree(spheres)).scene_rep()
+
+
+
+
 def SphereChain(top, count, radius):
     sphere = Sphere(
             diffuse=settings.default_diffuse,
@@ -32,7 +68,7 @@ def DiamondChain(top, count, radius):
         specular=settings.default_specular,
         shininess=settings.default_shininess,
         refraction=settings.default_refraction,
-    ), rotate=(-1,-1,0,90))])
+    ), rotate=(-1,-1,0,45))])
     return TaperedChain(diamond, top, count, radius / 2, radius / 2)
 
 class TaperedChain(SceneComponent):
--- generation/components/Chandelier.py
+++ generation/components/Chandelier.py
@@ -1,5 +1,6 @@
 from common import SceneComponent
-from components.Layers import PolygonLayer, SquareLayer, OctagonLayer
+from components.Layers import PolygonLayer, SquareLayer
+from components.Chain import TiltedSphereChain
 from shapes.primitives import Cube, Cone
 from shapes.transblock import TransBlock
 from shapes.tree import Tree
@@ -9,12 +10,8 @@ class Chandelier(SceneComponent):
         self.layers = [
             # SquareLayer((0, 4, 0), 1),
             # SquareLayer((0, 2, 0), 2),
-            PolygonLayer((0, 6, 0),  4, 3, 0.3),
-            PolygonLayer((0, 4, 0),  4, 4, 0.3),
-            PolygonLayer((0, 2, 0),  4, 5, 0.3),
             PolygonLayer((0, 0, 0),  4, 8, 0.3),
-            PolygonLayer((0, -2, 0), 4, 7, 0.3),
-            PolygonLayer((0, -4, 0), 4, 8, 0.3),
+            TiltedSphereChain((-1,0,-1), (1,1,1), 8)
         ] 
 
     def scene_rep(self):
