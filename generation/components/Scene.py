11e56629ecfea7378d2d70aafbba339818d9b30f
Paul Biberstein <biberstein.paul@gmail.com>
2021-12-14 21:38:03 -0500

    Move lights to context.py

--- generation/components/Scene.py
+++ generation/components/Scene.py
@@ -2,50 +2,8 @@ from common import SceneComponent
 from components.Chandelier import Chandelier
 from settings import settings
 import context
+from context import PointLight, DirectionalLight
 
-class PointLight(SceneComponent):
-    def __init__(self, id, color, position):
-        self.id = id
-        self.color = color
-        self.position = position
-
-    def scene_rep(self):
-        return ('<lightdata>\n'
-                '<id v="{}" />\n'
-                '<position x="{}" y="{}" z="{}"/>\n'
-                '<color r="{}" g="{}" b="{}"/>\n'
-                '</lightdata>\n'
-                ).format(
-                    self.id,
-                    self.position[0],
-                    self.position[1],
-                    self.position[2],
-                    self.color[0],
-                    self.color[1],
-                    self.color[2])
-                    
-
-class DirectionalLight(SceneComponent):
-    def __init__(self, id, color, direction):
-        self.id = id
-        self.color = color
-        self.direction = direction
-
-    def scene_rep(self):
-        return ('<lightdata>\n'
-                '<id v="{}" />\n'
-                '<type v="directional"/>\n'
-                '<color r="{}" g="{}" b="{}"/>\n'
-                '<direction x="{}" y="{}" z="{}"/>\n'
-                '</lightdata>\n'
-                ).format(
-            self.id,
-            self.color[0],
-            self.color[1],
-            self.color[2],
-            self.direction[0],
-            self.direction[1],
-            self.direction[2])
 
 class Camera(SceneComponent):
     def __init__(self, position, focus, up, height_angle):
@@ -83,24 +41,10 @@ class Scene(SceneComponent):
 
         self.env_map = settings.environment_map
 
-        # context.append_light(
-        #     PointLight(context.get_id(), (0.5, 0.5, 0.5), (10, 0, 0))
-        # )
-        # context.append_light(
-        #     PointLight(context.get_id(), (0.5, 0.5, 0.5), (0, 0, 10))
-        # )
-        # context.append_light(
-        #     PointLight(context.get_id(), (0.5, 0.5, 0.5), (-10, 0, 0))
-        # )
-        # context.append_light(
-        #     PointLight(context.get_id(), (0.5, 0.5, 0.5), (0, 0, -10))
-        # )
-        context.append_light(
-            DirectionalLight(context.get_id(), (0.5, 0.5, 0.5), (-1, -1, -1))
-        )
         self.chandelier = Chandelier()
 
     def scene_rep(self):
+        chandelier = self.chandelier.scene_rep()
         light_data = [light.scene_rep() for light in context.get_lights()]
         light_data = '\n'.join(light_data)
         return ('<scenefile>\n'
@@ -127,6 +71,6 @@ class Scene(SceneComponent):
                     self.env_map,
                     self.camera.scene_rep(),
                     light_data,
-                    self.chandelier.scene_rep())
+                    chandelier)
 
 
--- generation/context.py
+++ generation/context.py
@@ -1,3 +1,49 @@
+from common import SceneComponent
+
+class PointLight(SceneComponent):
+    def __init__(self, id, color, position):
+        self.id = id
+        self.color = color
+        self.position = position
+
+    def scene_rep(self):
+        return ('<lightdata>\n'
+                '<id v="{}" />\n'
+                '<position x="{}" y="{}" z="{}"/>\n'
+                '<color r="{}" g="{}" b="{}"/>\n'
+                '</lightdata>\n'
+                ).format(
+                    self.id,
+                    self.position[0],
+                    self.position[1],
+                    self.position[2],
+                    self.color[0],
+                    self.color[1],
+                    self.color[2])
+                    
+
+class DirectionalLight(SceneComponent):
+    def __init__(self, id, color, direction):
+        self.id = id
+        self.color = color
+        self.direction = direction
+
+    def scene_rep(self):
+        return ('<lightdata>\n'
+                '<id v="{}" />\n'
+                '<type v="directional"/>\n'
+                '<color r="{}" g="{}" b="{}"/>\n'
+                '<direction x="{}" y="{}" z="{}"/>\n'
+                '</lightdata>\n'
+                ).format(
+            self.id,
+            self.color[0],
+            self.color[1],
+            self.color[2],
+            self.direction[0],
+            self.direction[1],
+            self.direction[2])
+
 
 
 context = {
